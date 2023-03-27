b0266613cad2adb8eac11f74851545f6b050f5af
JISUlicious <ljshong@hanmail.net>
2023-03-27 18:33:29 +0900

    container-component 사용 (DayBox)

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -2,41 +2,52 @@
 creates Calendar component
 creates DayBox div with loop
 */
-import { monthNames } from "../params";
+import { monthNames, referenceColors } from "../params";
 import { dateToKey } from "../functions/dateToKey";
 import { DayBox } from "./DayBox";
-import { useCalendarContext } from "../contexts/CalendarContext";
+import { actionCreator, useCalendarContext, useCalendarDispatchContext } from "../contexts/CalendarContext";
 
 const numColumns = 13; // num months + day index col
 const numRows = 32; // num max days in a month + month index row
 
+const DayBoxContainer = (date, records, dispatch) => {
+  const key = dateToKey(date);
+  const {year, month, day} = date;
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const disabled = day > daysInMonth;
+  const indices = month === 0 || day === 0;
+  
+  const className = ` ${disabled ? "disabled" : ""}${indices ? "index" : ""}`;
+  // define style of DayBox
+  const record = !records ? null : records[key] ? records[key] : null;
+  const backgroundColor = disabled ? "dimgrey"
+    : record ? referenceColors[record.color]
+      : null;
+  const border = record && "1px solid black"
+  const gridArea = `${day + 1}/${month + 1}/${day + 2}/${month + 2}`
+  const style = {backgroundColor, border, gridArea};
+  // define action of DayBox
+  const onClick = disabled || indices ? null : () => {
+    dispatch(actionCreator.setDate(date));
+  };
+  // define content of DayBox
+  const content = month === 0 && day > 0 ? day
+    : day === 0 && month > 0 ? monthNames[month - 1]
+      : null;
+  
+  return (<DayBox key={key} className={className} style={style} onClick={onClick} content={content}/>);
+};
+
 export const Calendar = () => {
   
-  const {year} = useCalendarContext();
+  const {year, records} = useCalendarContext();
+  const dispatch = useCalendarDispatchContext();
   
   const boxes = [];
   for (let colCount = 0; colCount < numColumns; colCount++) {
     for (let rowCount = 0; rowCount < numRows; rowCount++) {
-      const boxDate = {year: year, month: colCount, day: rowCount};
-      const key = dateToKey(boxDate);
-
-      const daysInMonth = new Date(year, colCount, 0).getDate();
-      const disabled = rowCount > daysInMonth;
-      const indices = colCount === 0 || rowCount === 0;
-      const box = (
-        <DayBox
-          key={key}
-          date={boxDate}
-          gridArea={`${rowCount + 1}/${colCount + 1}/${rowCount + 2}/${
-            colCount + 2
-          }`}
-          disabled={disabled}
-          indices={indices}
-        >
-          {colCount === 0 && rowCount > 0 && rowCount}
-          {rowCount === 0 && colCount > 0 && monthNames[colCount - 1]}
-        </DayBox>
-      );
+      const date = {year: year, month: colCount, day: rowCount};
+      const box = DayBoxContainer(date, records, dispatch);
       boxes.push(box);
     }
   }
@@ -52,3 +63,4 @@ export const Calendar = () => {
     </div>
   );
 };
+
--- src/components/DayBox.js
+++ src/components/DayBox.js
@@ -1,34 +1,13 @@
-import { dateToKey } from "../functions/dateToKey";
-import { referenceColors } from "../params";
-import {
-  actionCreator,
-  useCalendarContext,
-  useCalendarDispatchContext
-} from "../contexts/CalendarContext";
 
-export const DayBox = ({date, gridArea, disabled, indices, children}) => {
-  
-  const dateKey = dateToKey(date);
-  
-  const {records} = useCalendarContext();
-  const calendarStateDispatch = useCalendarDispatchContext();
-  const record = records ? records : {};
 
+export const DayBox = ({className, style, onClick, content}) => {
+  
   return <div
-    className={`day-box ${disabled ? "disabled" : ""}`}
-    style={{
-      gridArea,
-      backgroundColor:
-        disabled ? "dimgrey"
-        : record[dateKey] ? referenceColors[record[dateKey].color]
-        : null,
-      border: record[dateKey] && "1px solid black"
-    }}
-    onClick={disabled || indices ? null : () => {
-      calendarStateDispatch(actionCreator.setDate(date));
-    }}
+    className={`day-box${className}`}
+    style={style}
+    onClick={onClick}
   >
-    {children}
+    {content}
   </div>
 };
 
