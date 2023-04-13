03acaa97d0e750bde4a25c201758b3538d2b0d2d
JISUlicious <ljshong@hanmail.net>
2023-04-13 18:35:00 +0900

    계정등록 추가

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,83 +1,14 @@
 import "../styles/App.scss";
 import "../styles/Auth.scss";
 import { useState } from "react";
-import { OAuthProviderName, signInEmail, signInOAuth, signUpEmail } from "../functions/auth";
-import { GoogleLoginButton, FacebookLoginButton } from "react-social-login-buttons";
+import { SignUp } from "./SignUp";
+import { SignIn } from "./SignIn";
 
 
 export const Auth = () => {
-  
-  const [inputId, setInputId] = useState(null);
-  const [inputPw, setInputPw] = useState(null);
-  
-  const onChangeId = (event) => {
-    setInputId(event.target.value);
-  };
-  const onChangePw = (event) => {
-    setInputPw(event.target.value);
-  };
-  const onSignIn = (event) => {
-    event.preventDefault();
-    signInEmail(inputId, inputPw)
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  
-  const onSocialLogin = (name) => {
-    signInOAuth(name)
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-  
-  const onSignUp = () => {
-    signUpEmail(inputId, inputPw)
-      .catch(error => {
-        console.log(error);
-      });
-  };
+  const [signUp, setSignUp] = useState(false);
 
-  
-  return (<div className="sign-in">
-    <div className="auth-wrapper">
-      <h1>Color Calendar</h1>
-      <form onSubmit={onSignIn}>
-        <label>
-          <input onChange={onChangeId} placeholder="ID" />
-        </label>
-        <br/>
-        <label>
-          <input onChange={onChangePw} placeholder="PW" type="password" />
-        </label>
-        <button type="submit">Sign In</button>
-      </form>
-      <GoogleLoginButton
-        onClick={() => onSocialLogin(OAuthProviderName.google)}
-        iconSize={20}
-        size={30}
-        style={{
-          fontSize: 14
-        }}
-        align={"center"}
-      />
-      <FacebookLoginButton
-        onClick={() => onSocialLogin(OAuthProviderName.facebook)}
-        iconSize={20}
-        size={30}
-        style={{
-          fontSize: 14
-        }}
-        align={"center"}
-      />
-      <button
-        className={"sign-up"}
-        onClick={onSignUp}
-      >
-        Sign up and make your own calendar
-      </button>
-      
-    </div>
-    
-  </div>);
+  return <>
+    { signUp ? <SignUp setSignUp={setSignUp}/> : <SignIn setSignUp={setSignUp}/>}
+    </>;
 };
---
+++ src/components/SignUp.js
@@ -0,0 +1,50 @@
+import "../styles/App.scss";
+import "../styles/Auth.scss";
+import { useState } from "react";
+import { signUpEmail } from "../functions/auth";
+
+export const SignUp = ({setSignUp}) => {
+  
+  const [inputId, setInputId] = useState(null);
+  const [inputPw, setInputPw] = useState(null);
+  
+  const onChangeId = (event) => {
+    setInputId(event.target.value);
+  };
+  const onChangePw = (event) => {
+    setInputPw(event.target.value);
+  };
+  const onSignUp = (event) => {
+    event.preventDefault();
+    signUpEmail(inputId, inputPw)
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  
+  const onBackToSignIn = () => {
+    setSignUp(null);
+  };
+  
+  return (<div className="sign-in">
+    <div className="auth-wrapper">
+      <h1>Color Calendar</h1>
+      <form onSubmit={onSignUp}>
+        <label>
+          <input onChange={onChangeId} placeholder="ID" required={true}/>
+        </label>
+        <label>
+          <input onChange={onChangePw} placeholder="PW" required={true} type="password" />
+        </label>
+          <button type="submit">Sign Up</button>
+      </form>
+      <button
+        className={"sign-up"}
+        onClick={onBackToSignIn}
+      >
+        Already have an account
+      </button>
+    </div>
+  
+  </div>);
+};
