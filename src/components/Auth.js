fb28f51f97f46eae576279c14d826878a385e34f
JISUlicious <ljshong@hanmail.net>
2023-04-18 18:12:39 +0900

    Auth.js signIn input 분리 signUp/signIn 기능

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,14 +1,26 @@
 import "../styles/App.scss";
 import "../styles/Auth.scss";
 import { useState } from "react";
-import { SignUp } from "./SignUp";
-import { SignIn } from "./SignIn";
+import { SocialSignIn } from "./SocialSignIn";
+import { AuthInput } from "./AuthInput";
 
 
 export const Auth = () => {
   const [signUp, setSignUp] = useState(false);
-
+  
   return <>
-    { signUp ? <SignUp setSignUp={setSignUp}/> : <SignIn setSignUp={setSignUp}/>}
+      <div className="sign-in">
+        <div className="auth-wrapper">
+          <h1>Color Calendar</h1>
+          <AuthInput isSignUp={ signUp } />
+          { !signUp && <SocialSignIn />}
+        <button
+          className={"sign-up"}
+          onClick={() => setSignUp(!signUp)}
+        >
+          {signUp ? "Already have an account" : "Sign up for new account"}
+        </button>
+        </div>
+      </div>
     </>;
 };
